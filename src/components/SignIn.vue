627a8f64a817a70e41857b0e668196902cf5a899
Chris Stair <ccstair@gmail.com>
2017-04-29 21:38:32 -0400

    everything else

---
+++ src/components/SignIn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="signin">
+    <input type="text" placeholder="Project Name" v-model="title">{{title}}</input><button v-on:click="titleProject(title)">Title Project</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signin',
+  data() {
+    return {
+      sups: 'Welcome to Your Vue.js App',
+    };
+  },
+  methods: {
+    titleProject(title) {
+      console.log(title);
+      return '';
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+// import Hello from '@/components/Hello';
+import SignIn from '@/components/SignIn';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'SignIn',
+      component: SignIn,
+    },
+  ],
+});
